3337e91a9b2d814e949fb53888e69c3ff3e09a6d
Michael Telford <micky_t@ymail.com>
2015-11-13 19:33:53 +0000

    Added code to database for inserts. Also moved the DB connection details to its own file.

--- database.rb
+++ database.rb
@@ -1,3 +1,4 @@
+require_relative "mongo_connection_details"
 require_relative "documents"
 require_relative "document"
 require_relative "url"
@@ -6,12 +7,9 @@ require 'mongo'
 # @author Michael Telford
 # Class modeling a DB connection and search engine related functionality.
 class Database
-    CONNECTION_DETAILS = {
-        :host   => "127.0.0.1",
-        :port   => "27017",
-        :db     => "crawler",
-        :uname  => "admin",
-        :pword  => "R5jUKv1fessb"
+    COMMON_INSERT_DATA = {
+        :date_added     => time_stamp,
+        :date_modified  => time_stamp
     }
     
     attr_reader :client
@@ -24,25 +22,39 @@ class Database
                                     :password => CONNECTION_DETAILS[:pword])
     end
     
-    # Insert Data.
+    # Create Data.
+    
+    def insert_url(url, source = nil)
+        data = {
+            :url            => url,
+            :source         => source,
+            :crawled        => false,
+            :date_crawled   => time_stamp
+        }
+        insert(:urls, data)
+    end
     
     def insert_urls(urls)
-        unless urls.respond_to?(:each)
-            insert_url(urls)
-        else
-            urls.each { |url| insert_url(url) }
+        return insert_url(urls) unless urls.respond_to?(:map)
+        urls.map do |url|
+            {
+                :url            => url,
+                :source         => source,
+                :crawled        => false,
+                :date_crawled   => time_stamp
+            }
         end
+        insert(:urls, urls, false)
+    end
+    
+    def insert_doc(doc)
     end
     
     def insert_docs(docs)
-        unless docs.respond_to?(:each)
-            insert_doc(docs)
-        else
-            docs.each { |doc| insert_doc(doc) }
-        end
+        #
     end
     
-    # Reteive Data.
+    # Retreive Data.
     
     # limit = 0 returns all uncrawled urls.
     def get_uncrawled_urls(limit = 0)
@@ -51,11 +63,28 @@ class Database
     def search(text, data = [:text])
     end
     
-    private
-    
-    def insert_url(url)
+    def stats
     end
     
-    def insert_doc(doc)
+    # Update Data.
+    
+    def update_crawled_url(url)
+    end
+    
+    private
+    
+    def time_stamp
+        Time.new.strftime("%Y-%m-%d %H:%M:%S").to_s
+    end
+    
+    def insert(collection, data, single = true)
+        raise "data must be a Hash" unless data.is_a?(Hash)
+        data.merge!(COMMON_INSERT_DATA)
+        if single
+            result = @client[collection.to_sym].insert_one(data)
+        else
+            result = @client[collection.to_sym].insert_many(data)
+        end
+        result.n # Return the number of inserted docs.
     end
 end
---
+++ mongo_connection_details.rb
@@ -0,0 +1,9 @@
+CONNECTION_DETAILS = {
+    :host           => "127.0.0.1",
+    :port           => "27017",
+    :db             => "crawler",
+    :uname          => "admin",
+    :pword          => "R5jUKv1fessb"
+}
+
+# Connection URL: mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/
