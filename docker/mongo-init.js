ee8518473aaa863285ff7bbc9372980b7098b9cb
Michael Telford <michael.telford@live.com>
2019-12-18 11:08:05 +0000

    Added docker files for local Mongo DB instance

---
+++ docker/mongo-init.js
@@ -0,0 +1,26 @@
+db.auth('rubyapp', 'abcdef')
+
+db = db.getSiblingDB('crawler')
+
+db.createUser({
+  user: 'rubyapp',
+  pwd: 'abcdef',
+  roles: [
+    {
+      role: 'root',
+      db: 'admin',
+    },
+  ],
+});
+
+db.createCollection("urls");
+db.createCollection("documents");
+
+db.urls.createIndex({ "url" : 1 }, { unique : true })
+db.documents.createIndex({ "url.url" : 1 }, { unique : true })
+db.documents.createIndex({
+  "text": "text",
+  "author": "text",
+  "keywords": "text",
+  "title": "text"
+})
